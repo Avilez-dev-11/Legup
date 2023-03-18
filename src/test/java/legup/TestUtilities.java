66fe121b2a9dc2a352c86fbe6804cb2b2746fa2f
Avilez-dev-11 <avilez@rpi.edu>
2023-03-17 21:28:55 -0400

    Update TestUtilities.java
    commented out the new global tester function as it is not compiling or passing the checkstyle at the moment.

--- src/test/java/legup/TestUtilities.java
+++ src/test/java/legup/TestUtilities.java
@@ -23,35 +23,35 @@ public final class TestUtilities {
     }
 
     // implementing a test function that will go through each puzzle rule to test the rules and have for testing purposes
-    public static void globalTester(){
-        /* BATTLESHIP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
-
-        // setting up a battelship tester for the adjacent ships contradiction rule test
-        AdjacentShipsContradictionRuleTest BattleshipContradictionTest = new AdjacentShipsContradictionRuleTest();
-        BattleshipContradictionTest.OrthogonalAdjacentTest();
-        BattleshipContradictionTest.InvalidOrthogonalAdjacentTest();
-
-        // setting up a battelship tester for the finish with ships direct rule test
-        FinishWithShipsDirectRuleTests BattleshipDirectTest = new FinishWithShipsDirectRuleTests();
-        BattleshipDirectTest.HorizontalValidTest();
-        BattleshipDirectTest.VerticaValidTest();
-        BattleshipDirectTest.HorizontalInvalidTest();
-        BattleshipDirectTest.VerticalInvalidTest();
-
-
-        /* LIGHTUP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
-
-        // setting up a lightup tester for the bulbs in path contradiction rule test
-        BulbsInPathContradictionRuleTest LightupBulbPathContradictionTest = new BulbsInPathContradictionRuleTest();
-        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInHorizontalPath();
-        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInVerticalPath();
-
-        // setting up a lightup tester for the cant lightup a cell contradiction rule test
-        CannotLightACellContradictionRuleTest LightupCantLightCellContradictionTest = new CannotLightACellContradictionRuleTest();
-        LightupCantLightCellContradictionTest.simpleCaseTest();
-
-        // setting up a lightup tester for the empty cell light direct rule test
-        EmptyCellinLightDirectRuleTest LightupEmptyCellTester = new EmptyCellinLightDirectRuleTest();
-    }
+//    public static void globalTester(){
+//        /* BATTLESHIP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
+//
+//        // setting up a battelship tester for the adjacent ships contradiction rule test
+//        AdjacentShipsContradictionRuleTest BattleshipContradictionTest = new AdjacentShipsContradictionRuleTest();
+//        BattleshipContradictionTest.OrthogonalAdjacentTest();
+//        BattleshipContradictionTest.InvalidOrthogonalAdjacentTest();
+//
+//        // setting up a battelship tester for the finish with ships direct rule test
+//        FinishWithShipsDirectRuleTests BattleshipDirectTest = new FinishWithShipsDirectRuleTests();
+//        BattleshipDirectTest.HorizontalValidTest();
+//        BattleshipDirectTest.VerticaValidTest();
+//        BattleshipDirectTest.HorizontalInvalidTest();
+//        BattleshipDirectTest.VerticalInvalidTest();
+//
+//
+//        /* LIGHTUP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
+//
+//        // setting up a lightup tester for the bulbs in path contradiction rule test
+//        BulbsInPathContradictionRuleTest LightupBulbPathContradictionTest = new BulbsInPathContradictionRuleTest();
+//        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInHorizontalPath();
+//        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInVerticalPath();
+//
+//        // setting up a lightup tester for the cant lightup a cell contradiction rule test
+//        CannotLightACellContradictionRuleTest LightupCantLightCellContradictionTest = new CannotLightACellContradictionRuleTest();
+//        LightupCantLightCellContradictionTest.simpleCaseTest();
+//
+//        // setting up a lightup tester for the empty cell light direct rule test
+//        EmptyCellinLightDirectRuleTest LightupEmptyCellTester = new EmptyCellinLightDirectRuleTest();
+//    }
 
 }
