853af369147825c620eff2b3c1a2bdd087997c3a
Avilez-dev-11 <avilez@rpi.edu>
2023-03-17 21:22:49 -0400

    Update TestUtilities.java
    made a globaltester function that will test all given tests based on the rules

--- src/test/java/legup/TestUtilities.java
+++ src/test/java/legup/TestUtilities.java
@@ -6,6 +6,11 @@ import edu.rpi.legup.model.tree.Tree;
 import edu.rpi.legup.model.tree.TreeNode;
 import edu.rpi.legup.model.tree.TreeTransition;
 import edu.rpi.legup.save.InvalidFileFormatException;
+import puzzles.battleship.rules;
+import puzzles.lightup.rules;
+import puzzles.lightup.rules.CannotLightACellContradictionRuleTest;
+import puzzles.nurikabe.rules;
+import puzzles.shorttruthtable.rules;
 
 public final class TestUtilities {
     public static void importTestBoard(String fileName, Puzzle puzzle) throws InvalidFileFormatException {
@@ -16,4 +21,37 @@ public final class TestUtilities {
         TreeTransition transition = new TreeTransition(rootNode, board);
         rootNode.getChildren().add(transition);
     }
+
+    // implementing a test function that will go through each puzzle rule to test the rules and have for testing purposes
+    public static void globalTester(){
+        /* BATTLESHIP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
+
+        // setting up a battelship tester for the adjacent ships contradiction rule test
+        AdjacentShipsContradictionRuleTest BattleshipContradictionTest = new AdjacentShipsContradictionRuleTest();
+        BattleshipContradictionTest.OrthogonalAdjacentTest();
+        BattleshipContradictionTest.InvalidOrthogonalAdjacentTest();
+
+        // setting up a battelship tester for the finish with ships direct rule test
+        FinishWithShipsDirectRuleTests BattleshipDirectTest = new FinishWithShipsDirectRuleTests();
+        BattleshipDirectTest.HorizontalValidTest();
+        BattleshipDirectTest.VerticaValidTest();
+        BattleshipDirectTest.HorizontalInvalidTest();
+        BattleshipDirectTest.VerticalInvalidTest();
+
+
+        /* LIGHTUP TESTER FOR THE GIVEN RULE TESTS IN PUZZLES FOLDER */
+
+        // setting up a lightup tester for the bulbs in path contradiction rule test
+        BulbsInPathContradictionRuleTest LightupBulbPathContradictionTest = new BulbsInPathContradictionRuleTest();
+        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInHorizontalPath();
+        LightupBulbPathContradictionTest.BulbsInPathContradictionRule_LightInVerticalPath();
+
+        // setting up a lightup tester for the cant lightup a cell contradiction rule test
+        CannotLightACellContradictionRuleTest LightupCantLightCellContradictionTest = new CannotLightACellContradictionRuleTest();
+        LightupCantLightCellContradictionTest.simpleCaseTest();
+
+        // setting up a lightup tester for the empty cell light direct rule test
+        EmptyCellinLightDirectRuleTest LightupEmptyCellTester = new EmptyCellinLightDirectRuleTest();
+    }
+
 }
